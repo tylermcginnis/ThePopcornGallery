781fe4ea1f86392a60730d9768993ff7a40fc49b
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-05-27 19:04:15 -0600

    Full Commit
    Full source code for TPG

---
+++ data.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html> 
+	<head> 
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> <!-- VERIFY THIS ONCE LIVE --> 
+		<link rel="stylesheet" type="text/css" href="css/main_style.css" />
+		<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+		<script src="js/jquery.js"></script>
+		<script src="js/jquery.roundabout.js"></script>
+		<link rel="stylesheet" href="css/css_signup/reset.css">
+		<link rel="stylesheet" href="css/css_signup/animate.css">
+		<link rel="stylesheet" href="css/css_signup/styles_new.css">
+		<script>
+			$(document).ready(function() {
+			$('#the_team > ul').roundabout();
+			});			
+		</script>		
+		<title> The Popcorn Gallery: The new way to be social on the videos you want to watch, when you want to watch them.  </title> 
+	</head> 
+	
+	<body> 
+		<div id="full_container">
+			<div class="content_container">
+				<div id="top_links">
+					<ul> 
+						<li><a href="#home"> Home </a> </li>
+						<li><a href="#section_two"> FAQ </a> </li>
+						<li><a href="#section_three"> Sign Up </a> </li> 
+						<li><a href="#the_team"> The Team </a> </li> 
+					</ul>
+				</div> 
+			<!-- START HOME --> 
+				<div id="home">
+				<div id="logo"></div> 
+				<div id="slogan">
+					<h1> Change the way you experience on demand content. </h1> 
+				</div> 
+				<div id="second_slogan"> 
+					<h3> The Popcorn Gallery is the new way to be social on the videos you want to watch, when you want to watch them.  </h3> 
+				</div> 
+				<div class="left_bubbles">
+					<div class="bubble"> The Popcorn Gallery bridges time and space... every single day. <br /><br /> - TPG Team </div>
+					<div class="bubble_two"> It's kind of like Mystery Theater 3000, but from people I care about. And, I can still hear the movie. <br /><br /> -Beta Tester </div> 
+				</div> 
+				<div class="right_bubbles">
+					<div class="bubble_three"> I'm now unashamed to admit I enjoy watching Downton Abbey with my wife. In fact, with TPG, I actually look forward to it.<br /><br /> -Beta Tester </div>
+					<div class="bubble_four"> I'll be able to read relavent commentary without listening to the audio track during the movie? Where can I get it?<br /><br /> -Online Movie Reviewer  </div> 
+				</div> 
+				<div id="iphone"> </div> 
+			</div>  
+			<!-- END HOME --> 
+			<br><br>
+			<!-- START SECTION_TWO --> 
+				<div id="section_two">
+					<div id="top_links">
+					<ul> 
+						<li><a href="#home"> Home </a> </li>
+						<li><a href="#section_two"> FAQ </a> </li>
+						<li><a href="#section_three"> Sign Up</a> </li> 
+						<li><a href="#the_team"> The Team </a> </li> 
+					</ul>
+				</div> 
+				<div id="logo"></div> 
+				<div id="slogan_faq">
+					<h1> Frequently Asked Questions </h1> 
+				</div> 
+				<div id="faq_container">
+					<div id="question"> When will The Popcorn Gallery be Available? </div> 
+						<div id="answer"> As soon as possible. If you have an iPhone, that means April 1st.</div> 
+					
+					<div id="question"> Where did the name come from? </div> 
+						<div id="answer"> It's a reference to the peanut gallery back in Shakespeare's time. Popcorn instead of peanuts, in reference to modern film. </div> 
+					
+					<div id="question"> I'm interested in being a BETA tester, how can I make that happen? </div> 
+						<div id="answer"> Hit up the "Sign Up" page below. Your chances go up if you fill it out hundreds of times. Wait. That's not true. </div> 
+					
+					<div id="question"> Have you thought about _____ as a feature for The Popcorn Gallery? </div> 
+						<div id="answer"> Yes. Or maybe. Sometimes No? We love to hear suggestions from our users. We're building this for you, so let us know what you want to see on twitter @thepopcornteam.</div> 
+					<div id="question"> Sounds great. How much is this going to cost me? </div> 
+						<div id="answer"> Nothing. We promise. The Popcorn Gallery will always be free to users.</div> 
+					<div id="question"> Is this going to spam my Facebook friends or Twitter stream?</div> 
+						<div id="answer"> Nope. We'd love for you to let your friends know all about us, but we're not interested in posting anything to your wall/stream.</div> 	
+				</div> 
+				
+				</div> 
+			<!-- END SECTION_TWO --> 
+			<br><br>
+			<!-- START SECTION_THREE --> 
+				<div id="section_three">
+				<div id="top_links">
+					<ul> 
+						<li><a href="#home"> Home </a> </li>
+						<li><a href="#section_two"> FAQ</a> </li>
+						<li><a href="#section_three"> Sign Up </a> </li> 
+						<li><a href="#the_team"> The Team </a> </li> 
+					</ul>
+				</div> 
+				<div id="logo"></div> 
+					<div id="container">
+							<?php
+								$host = 'localhost'; 
+								$name = 'photoann_tyler'; 
+								$pw = 'ichiro'; 
+								$db_name = 'photoann_popcorn'; 
+								$connect = mysql_connect($host, $name, $pw); 
+								mysql_select_db($db_name); 
+		
+								$name = $_POST['email']; 
+		
+									if(empty($name)){
+										echo "
+										<span class='thankyou'>Oops! <br><br> Looks like you forgot to add a name. Go back and re-try.  </span> <br><br>"; 
+									} 
+			
+									else{			
+										mysql_query("INSERT INTO registration(name) VALUES('$name')") OR die(mysql_error()); 
+										echo "<span class='thankyou'>Thank you! <br><br> We look forward to seeing you on the 8th! </span> <br><br>"; 
+									} 
+							?> 
+					</div>
+				</div> 
+			<!-- END SECTION_THREE --> 
+			<br><br>
+			
+			<!-- START THE_TEAM SECTION --> 
+				<div id="the_team"> 
+					<div id="top_links">
+					<ul> 
+						<li><a href="#home"> Home </a> </li>
+						<li><a href="#section_two"> FAQ</a> </li>
+						<li><a href="#section_three"> Sign Up </a> </li> 
+						<li><a href="#the_team"> The Team </a> </li> 
+					</ul>
+				</div>
+				<div id="logo"></div> 				
+					<h1> The Team </h1> 
+					<ul> 
+						<li><img src="images/team/jake.jpg" /> Jake Lingwall <br> Back End Developer </li> 
+						<li><img src="images/team/james.jpg" /> James Deibert <br> IOS Developer </li> 
+						<li><img src="images/team/matth.jpg" /> Matt Donohue <br> Accountant </li> 
+						<li><img src="images/team/mike.jpg" /> Michael Kirby <br> Marketer </li> 
+						<li><img src="images/team/tyler_mcginnis.jpg" /> Tyler McGinnis <br> Web Developer</li> 
+					</ul> 
+				</div> 
+			<!-- END THE_TEAM SECTION --> 
+			
+			</div>	
+		</div> 
+	</body> 
+</html> 
+
+
+
+
